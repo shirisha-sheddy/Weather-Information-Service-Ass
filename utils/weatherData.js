936c1c61150840d9f8b8dc73a7b318f73dbce496
shirisha-sheddy <shirishasheddy963@gmail.com>
2024-08-04 14:59:01 +0530

    weather app

---
+++ utils/weatherData.js
@@ -0,0 +1,20 @@
+const request = require("request");
+
+const openWeatherMap = {
+  BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+  SECRET_KEY: "37ecd53083968ee68704acda2179296c",
+};
+
+const weatherData = (address, callback) => {
+  const url =
+    openWeatherMap.BASE_URL + encodeURIComponent(address) + "&APPID=" + openWeatherMap.SECRET_KEY;
+  console.log(url);
+  request({ url, json: true }, (error, data) => {
+    if (error) {
+      callback(true, "Unable to fetch data, Please try again. " + error);
+    }
+    callback(false, data?.body);
+  });
+};
+
+module.exports = weatherData;
